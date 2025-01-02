bbcf89d5036fef8fa1d7c9f96610fbee135e4bae
roblopes7 <42593177+roblopes7@users.noreply.github.com>
2025-01-02 09:51:44 -0300

    Finalizado primeira versao

--- README.md
+++ README.md
@@ -1,27 +1,16 @@
-# PortifolioAngular
+<h1 align="center">
+  <img src="https://vidafullstack.com.br/wp-content/uploads/2020/07/angular.png" alt="" width="150">
+  <br>
+    Projeto Prático: Portfólio
+  <br>
+  https://github.com/roblopes7/portifolio-angular/browser/
+</h1>
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.1.
 
-## Development server
+## :custard: Acesse o GhPages
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+<strong>Link:</strong> https://github.com/roblopes7/portifolio-angular/browser/
 
-## Code scaffolding
+---
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+Baseado no curso de Dener Troquatte.
--- src/app/modules/portifolio/components/experiences/experiences.component.ts
+++ src/app/modules/portifolio/components/experiences/experiences.component.ts
@@ -12,14 +12,37 @@ export class ExperiencesComponent {
   public arrayExperiences = signal<IExperiences[]>([
     {
       summary: {
-        strong: 'Analista Desenvolvedor',
+        strong: 'Analista Desenvolvedor Fullstack',
         p: 'Zap Software & Soluções | DEZ/2021 - Presente',
       },
-      text: `<p>  Analista Desenvolvedor para novos projetos usando SpringFramework,
-            Angular(13), Android com Jetpack Compose(Kotlin).
-            CodeReview, pair programming, análises(técnicas e funcionais) e
-            gerenciamento técnico de novas soluções.
-      </p>
+      text: `
+      <p><strong>Resumo:</strong></p>
+      <p>Atuei como analista e desenvolvedor fullstack, focado em projetos com Angular 13 e Spring,
+      também em ERP (Java Swing) da minha primeira experiência, participando de code reviews e participação em análises (técnicas e funcionais).
+      Também trabalhei no desenvolvimento de um projeto para atualização de licenças do ERP via filacom RabbitMQ.</p>
+      <p>Implementei novos módulos em um app Android nativo usando Jetpack Compose (Kotlin),
+      em uma aplicação offline com sincronização quando online e integração de assinatura digital para confirmação
+      de participantes. A API utilizada para o app seguiu uma arquitetura limpa, e fui responsável pela implementação
+      dos novos módulos e funcionalidades.</p>
+
+      <p><strong>Tecnologias e Ferramentas:</p></strong>
+
+      <p>Back-End: Java (8, 17 e 19), Spring Framework.</p>
+      <p>Front-End: Angular 13.</p>
+      <p>Banco de Dados:Postgres.</p>
+      <p>Infraestrutura: RabbitMQ, Azure Boards, Git.</p>
+      <p>Testes: Mockito e Junit 5.</p>
+
+      <p><strong>Desafios e Soluções:</p></strong>
+
+       <p>Desafio: Desenvolver uma aplicação Android nativa com funcionalidade offline e
+       sincronização de dados quando online, além de integrar assinatura digital na aplicação.</p>
+
+      <p>Solução: Usei Jetpack Compose (Kotlin) para criar uma interface intuitiva e implementei
+      a sincronização de dados com a API (arquitetura limpa).
+      A integração de assinatura digital foi realizada com uma biblioteca de terceiros,
+      garantindo a confirmação de pedidos enquanto o sistema estava offline e realizando a
+      sincronização quando a internet estava disponível.</p>
       `,
     },
     {
@@ -27,10 +50,33 @@ export class ExperiencesComponent {
         strong: 'Desenvolvedor Back-end',
         p: 'Gempe Soluções em Tec. da Info. | MAI/2021 - DEZ/2021',
       },
-      text: `<p>   Desenvolvimento de API Java(SpringFramework) e AngularJS, MySql,
-              atuando diretamente com o cliente.Também no sistema back-office com
-              PHP,Docker,Node e SQS.
-      </p>
+      text: `
+      <p><strong>Resumo:</strong></p>
+        <p>Contratado pela Gempe para atuar como desenvolvedor alocado em um cliente,
+        trabalhei no desenvolvimento e manutenção de APIs em Java (Spring Framework) e AngularJS,
+        além de um sistema back-office utilizando PHP, Docker, Node.js e SQS. Durante o projeto, contribui com integrações,
+        geração de relatórios e alterações no sistema de atendimento, além de auxiliar na implantação do código em
+        ambientes de homologação.</p>
+
+        <p><strong>Tecnologias e Ferramentas:</p></strong>
+
+        <p>Back-End: Java 11 (Spring Framework), PHP, Node.js.</p>
+        <p>Front-End: AngularJS.</p>
+        <p>Banco de Dados: MySQL (queries manuais para relatórios).</p>
+        <p>Infraestrutura: Docker, Azure DevOps, Jenkins, SQS.</p>
+        <p>Metodologias: Scrum e reuniões diárias com múltiplas equipes.</p>
+
+        <p><strong>Desafios e Soluções:</p></strong>
+        <p>No início, a atuação envolveu o estudo de um grande sistema de condomínio em Java 11,
+        com arquitetura customizada e sem uso do Spring, para compreender integrações essenciais para o sistema de atendimento.
+        Posteriormente, foquei no sistema de atendimento, realizando alterações no AngularJS,
+        adicionando funcionalidades no back-end com Spring e ajustando relatórios SQL sob demanda,
+        atendendo às necessidades do cliente.</p>
+
+        <p>Um dos maiores desafios foi lidar com a complexidade das integrações e a necessidade de validar logins
+        por meio do sistema principal, antes de acessar o sistema de atendimento.
+        Além disso, no período final do contrato, fui realocado para um sistema back-office,
+        onde atuei na manutenção e melhorias em PHP, Docker e Node.js.</p>
       `,
     },
     {
@@ -38,11 +84,30 @@ export class ExperiencesComponent {
         strong: 'Desenvolvedor Java JR',
         p: 'Zap Software & Soluções | NOV/2019 - MAI/2021',
       },
-      text: `<p> Desenvolvimento com Java 8 em manutenção de ERP,utilizando da
-         biblioteca Swing, persistência de dados com Hibernate,controle de
-        versões SVN,relatórios com Jasper Reports e Scrum para organização de
-        sprints.
+      text: `
+      <p><strong>Resumo:</strong></p>
+
+      <p>
+        Atuei no desenvolvimento e manutenção de um ERP com múltiplos módulos,
+        abrangendo áreas como Comercial (pedidos, notas fiscais), Produção, Transporte (CT-e, MDF-e) e Financeiro (contas a pagar e receber) entre outros.
+        A maior parte do trabalho consistia em resolver problemas identificados pelos clientes ou pela gerência, além de implementar novas funcionalidades dentro do ERP.
+        Trabalhei em equipe utilizando metodologias ágeis (Scrum), com participação ativa em sprints e revisão de código.
       </p>
+       <p><strong>Tecnologias e Ferramentas:</strong></p>
+        <p>Linguagens e Frameworks: Java 8, Hibernate.</p>
+        <p>Interface de Usuário: Java Swing.</p>
+        <p>Banco de Dados: PostgreSQL (queries SQL diretas para otimização).</p>
+        <p>Relatórios: Jasper Reports.</p>
+        <p>Controle de Versão: SVN (Tortoise).</p>
+        <p>Metodologias: Scrum.</p>
+       <p><strong>Desafios e Soluções:</strong></p>
+       <p>Um dos projetos mais desafiadores foi a implementação de uma nova funcionalidade para gerenciar planos contábeis e gerenciais dentro do ERP. O objetivo era permitir que contas, como parcelas a pagar ou a receber, fossem associadas a múltiplos planos contábeis e, dentro deles, rateadas novamente em planos gerenciais, obedecendo diversas regras de negócio.</p>
+       <p>A complexidade residia em:</p>
+        <ul>
+          <li><p>Criar uma interface genérica e uma tela modular capaz de atender a diferentes módulos (pedidos, notas fiscais, contas a pagar/receber) com uma única implementação.</p></li>
+        </ul>
+       <p>O projeto exigiu análise detalhada dos diferentes pontos do ERP para integração, cálculos de rateio precisos e extensiva validação para assegurar a conformidade das regras. A solução foi desenvolvida com uma arquitetura baseada em interfaces, possibilitando modularidade e reuso de código.</p>
+
       `,
     },
   ]);
